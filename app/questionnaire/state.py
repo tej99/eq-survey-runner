b4a0269bcff1ecc6e72bd1993ad7694b10b45c61
Warren Bailey <warren@warrenbailey.net>
2016-08-24 11:28:29 +0100

    Adding documentation

--- app/questionnaire/state.py
+++ app/questionnaire/state.py
@@ -1,3 +1,16 @@
+'''
+  Everything in this class and the following packages is potentially serialized to the database and
+  therefore can cause issues if the code changes between deployments and sessions remain in the database.
+
+
+  schema - the entire schema (once it's parsed) this is needed as repeated elements modifies the schema, so the modified
+  version needs to be recovered
+
+  questionnaire_state - the doubly linked listed, i.e. the Node object, its associated Block State and all the child
+  states
+
+'''
+
 
 class State(object):
     def __init__(self, schema, current_node, first_node, archive, submitted_at, valid_locations):

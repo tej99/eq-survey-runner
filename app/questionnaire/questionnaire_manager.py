eeba8318c9c5c40d1ec1370cf487350b7d281145
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2016-11-02 11:56:11 +0000

    only append location if not exists

--- app/questionnaire/questionnaire_manager.py
+++ app/questionnaire/questionnaire_manager.py
@@ -72,7 +72,8 @@ class QuestionnaireManager(object):
             # Store answers in QuestionnaireStore
             questionnaire_store = get_questionnaire_store(current_user.user_id, current_user.user_ik)
 
-            questionnaire_store.visited_blocks.append(location)
+            if location not in questionnaire_store.visited_blocks:
+                questionnaire_store.visited_blocks.append(location)
 
             questionnaire_store.save()
 

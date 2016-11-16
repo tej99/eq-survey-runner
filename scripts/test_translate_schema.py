4d159ed858c5ac0d801995edce95a5c837503c35
Darrell Cox <darrellcox@Darrells-MacBook-Pro.local>
2016-11-16 10:36:21 +0000

    trial add stuff

--- scripts/test_translate_schema.py
+++ scripts/test_translate_schema.py
@@ -1,261 +1,170 @@
 import json
 
 
-with open('/Users/darrellcox/projects/eq-survey-runner/app/data/1_0112.json', 'r') as jsonData:
+with open('/Users/darrellcox/projects/eq-survey-runner/app/data/1_0112.json', 'r', encoding="utf8") as jsonData:
     data = json.load(jsonData)
 
-    #jsonData.close()
+    jsonData.close()
 
-# Prints off everything in the title area
-def first_layer():
-    # var = data['title']
-    # var2 = data['description']
-    # var3 = data['introduction']['description'] + '\n'
 
-    var = ''
-    var += data['title'] + '\n'
-    var += data['description'] + '\n'
-    var += data['introduction']['description'] + '\n'
-    var += data['introduction']['information_to_provide'][0] + '\n'
-    var += data['introduction']['information_to_provide'][1] + '\n'
-    var += data['introduction']['information_to_provide'][2] + '\n'
-    var += data['introduction']['information_to_provide'][3]
-    print var
+# All translatable text from first layer of file
+def get_header_text():
+    # The list of strings we're going to build up
+    the_list = []
 
-    # print var + '\n'
-    # print var2 + '\n'
-    # print var3 + '\n'
-    #
-    # for start in data['introduction']:
-    #     print (data['information_to_provide'])
+    the_list.append("header: " + data['title'])
+    the_list.append("header: " + data['description'])
+    the_list.append("header: " + data['introduction']['description'])
 
-    # print variable + write to a file
+    for value in data['introduction']['information_to_provide']:
+        the_list.append("header: " + value)
 
+    return the_list
+
+
+# All text from 'blocks' with given keys
+def get_blocks_text(keys):
+    # The list of strings we're going to build up
+    the_list = []
+
+    for key in keys:
+
+        for group in data['groups']:
+            for block in group['blocks']:
+                if block.get(key) != None and block.get(key) != '':
+                    the_list.append("blocks: " + block.get(key))
+
+                return the_list
+
+
+# All text from 'sections' with given keys
+def get_sections_text(keys):
+
+    # The list of strings we're going to build up
+    the_list = []
+
+    for key in keys:
+
+        for group in data['groups']:
+            for block in group['blocks']:
+                for section in block['sections']:
+                    # Check we've actually found something
+                    if section.get(key) != None and section.get(key) != '':
+                        the_list.append("sections: " + section.get(key))
+
+                    return the_list
+
+
+# All text from 'questions' with given keys
+def get_questions_text(keys):
+    # The list of strings we're going to build up
+    the_list = []
+
+    for key in keys:
+
+        for group in data['groups']:
+            for block in group['blocks']:
+                for section in block['sections']:
+                    for question in section['questions']:
+                        # Check we've actually found something
+                        if question.get(key) != None and question.get(key) != '':
+                            the_list.append("questions: " + question.get(key))
+
+                        return the_list
+
+
+# All text from 'answers' with given keys
+def get_answers_text(keys):
+    # The list of strings we're going to build up
+    the_list = []
+
+    for key in keys:
+
+        for group in data['groups']:
+            for block in group['blocks']:
+                for section in block['sections']:
+                    for question in section['questions']:
+                        for answer in question['answers']:
+                            # Check we've actually found something
+                            if answer.get(key) != None and answer.get(key) != '':
+                                the_list.append("answers" + answer.get(key))
+
+                            return the_list
+
+
+# All text from 'messages' with given keys
+def get_validation_message_text():
+    # The list of strings we're going to build up
+    the_list = []
+
+    for group in data['groups']:
+        for block in group['blocks']:
+            for section in block['sections']:
+                for question in section['questions']:
+                    for answer in question['answers']:
+
+                        if 'validation' in answer:  # Ensure key is available!
+
+                            for key, value in answer['validation']['messages'].items():
+                                the_list.append("messages: " + value)
+
+                                return the_list
+
+
+# Wrapper function which generates the output file
+def get_translatable_text():
+
+    keys = [
+      'description',
+      'guidance',
+      'label',
+      'title'
+    ]
+
+    header_text = get_header_text()
+    blocks_text = get_blocks_text(keys)
+    sections_text = get_sections_text(keys)
+    question_text = get_questions_text(keys)
+    validation_text = get_validation_message_text()
+
+    combined = header_text + blocks_text + sections_text + question_text + validation_text
+
+    # Convert to set to remove all duplicates
+    unique_text = set(header_text + blocks_text + sections_text + question_text + validation_text)
+
+    # Convert back to list to sort
+    sorted_list = list(unique_text)
+    sorted_list.sort()
+
+    return sorted_list
+
+
+def output_to_file(text_list):
+    # Output the list - this is just for testing! Please remove after!
+    for line in text_list:
+        print("%s" % line)
+
+    # Dump the output to a file
     test_file = open('test.txt', 'w')
 
-    test_file.write(var)
+    for line in text_list:
+        test_file.write("%s\n" % line)
 
-first_layer()
-
-
-# def first_id():
-#     output = []
-#     for first in data:
-#         for row in data[first]:
-#             print(data[first][row])
-#             output.append(data[first][row])
+    test_file.close()
 
 
+def run():
+    text = get_translatable_text()
+
+    output_to_file(text)
 
 
-
-# try a for loop
-# prints off all data in groups
-# def groups_data():
-#     if data['groups'] == []:
-#         print 'No Data!'
-#     else:
-#         for rows in data['groups']:
-#             print rows['blocks']
-
-    # if data['groups'] == []:
-    #     print 'No Data!'
-    # else:
-    # for group in data['groups']:
-
-
-# All ID in blocks
-def blocks_id():
-    for group in data['groups']:
-        for block in group['blocks']:
-            #print (block.get('id'))
-            print (block.get('title'))
-            test_file = open('test.txt', 'a')
-            test_file.write(block.get('title') + '\n')
-
-# All ID in sections
-def sections_id():
-    for group in data['groups']:
-        for block in group['blocks']:
-            for section in block['sections']:
-                #print (section.get('id'))
-                print (section.get('title', 'description'))
-                test_file = open('test.txt', 'a')
-                test_file.write(section.get('title', 'description') + '\n')
-
-# All ID in questions
-def questions_id():
-    for group in data['groups']:
-        for block in group['blocks']:
-            for section in block['sections']:
-                for question in section['questions']:
-                    #print (question.get('id'))
-                    print (question.get('title', str('description')))
-                    test_file = open('test.txt', 'a')
-                    test_file.write(question.get('title', 'description') + '\n')
-
-#All ID in answers
-def answers_id():
-    for group in data['groups']:
-        for block in group['blocks']:
-            for section in block['sections']:
-                for question in section['questions']:
-                    for answer in question['answers']:
-                        #print (answer.get('id'))
-                        print (answer.get('label'))
-                        test_file = open('test.txt', 'a')
-                        test_file.write(answer.get('label') + '\n')
-
-# Prints ALL validation messages
-def validation_messages():
-    for group in data['groups']:
-        for block in group['blocks']:
-            for section in block['sections']:
-                for question in section['questions']:
-                    for answer in question['answers']:
-                        for validate, value in answer['validation']['messages'].items():
-                            print (value)
-                            test_file = open('test.txt', 'a')
-                            test_file.write(value + '\n')
-
-
-blocks_id()
-sections_id()
-questions_id()
-answers_id()
-validation_messages()
-
-
-
-
-
-
-# if not data['groups']:
-#     print 'No Data!'
-# else:
-#     for rows in data['groups']:
-#         var += data['blocks']
-
-
-# for var1 in jsonData:
-#     for attribute, value in var1.iteritems():
-#       var += attribute, value
-
-
-# var += "\n".join(data['introduction']['information_to_provide'][0:])
-
-
-# Extract all data that can be translated
-
-# print(data["mime_type"])
-# print(data["questionnaire_id"])
-# print(data["schema_version"])
-# print(data["survey_id"])
-# print(data["title"])
-# print(data["description"])
-# print(data["theme"])
-# print(data["introduction"])
-# print(data['introduction']['description'])
-# print(data['introduction']['information_to_provide'][0:])
-# print(data["eq_id"])
-
-# prints section 1 (Reporting Period)
-
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['answers'][1]['label'])
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['answers'][1]['validation'])
-#
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][0]['sections'][0]['questions'][0]['title'])
-
-
-# prints section 2 (Retail Turnover)
-
-# print(data['groups'][0]['blocks'][1]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][1]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][1]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][1]['sections'][0]['questions'][0]['title'])
-#
-# print(data['groups'][0]['blocks'][1]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][1]['title'])
-
-
-# prints section 3 (Internet Sales)
-
-
-# print(data['groups'][0]['blocks'][2]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][2]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][2]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][2]['sections'][0]['questions'][0]['title'])
-#
-# print(data['groups'][0]['blocks'][2]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][2]['title'])
-
-
-# prints section 4 (Changes in total retail turnover)
-
-# print(data['groups'][0]['blocks'][3]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][3]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][3]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][3]['sections'][0]['questions'][0]['title'])
-#
-# print(data['groups'][0]['blocks'][3]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][3]['title'])
-
-
-# prints section 5 (Employees)
-
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][1]['label'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][1]['validation'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][2]['label'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][2]['validation'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][3]['label'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][3]['validation'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][4]['label'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['answers'][4]['validation'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][4]['sections'][0]['questions'][0]['title'])
-#
-# print(data['groups'][0]['blocks'][4]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][4]['title'])
-
-
-# prints section 6 (Changes in employee figures)
-
-# print(data['groups'][0]['blocks'][5]['sections'][0]['questions'][0]['answers'][0]['label'])
-# print(data['groups'][0]['blocks'][5]['sections'][0]['questions'][0]['answers'][0]['validation'])
-#
-# print(data['groups'][0]['blocks'][5]['sections'][0]['questions'][0]['description'])
-# print(data['groups'][0]['blocks'][5]['sections'][0]['questions'][0]['title'])
-#
-# print(data['groups'][0]['blocks'][5]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][5]['title'])
-
-
-
-# prints last part ( main titles?)
-# print(data['groups'][0]['blocks'][0]['sections'][0]['title'])
-# print(data['groups'][0]['blocks'][0]['title'])
+# Entry point for entire script for now...
+run()
 
 
 # print(json.dumps(data))
 # print data
-
-
-
-
 
 #     data["mime_type"] = 'something/test'
 #
@@ -263,18 +172,4 @@ validation_messages()
 #
 #     target_file = open('small_new.json', 'w')
 #
-#     json_dump = json.dumps(data, indent=4, separators=(',', ': '))
-#
-#     target_file.write(json_dump)
-#
-#     #json_dump = json.dumps(data, indent=4, separators=(',', ': '))
-#
-# #with open("file.txt", "w") as att_file:
-# #    att_file.write(data)
-
-
-    # with open('data.json', 'w') as f:
-    #     json.dump(data, f)
-
-# Reading data back
-
+#     json_dump = json.dumps(data,
---
+++ scripts/test_translate_schema_v1.py
@@ -0,0 +1,224 @@
+import json
+
+
+with open('/Users/darrellcox/projects/eq-survey-runner/app/data/1_0112.json', 'r', encoding="utf8") as jsonData:
+    data = json.load(jsonData)
+
+    jsonData.close()
+
+
+# All translatable text from first layer of file
+def get_header_text():
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  the_list.append("header: " + data['title'])
+  the_list.append("header: " + data['description'])
+  the_list.append("header: " + data['introduction']['description'])
+
+  for value in data['introduction']['information_to_provide']:
+    the_list.append("header: " + value)
+
+  return the_list
+
+
+
+# All text from 'blocks' with given keys
+def get_blocks_text(keys):
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  for key in keys:
+
+    for group in data['groups']:
+        for block in group['blocks']:
+
+          if block.get(key) != None and block.get(key) != '':
+            the_list.append("blocks: " + block.get(key))
+
+  return the_list
+
+
+
+# All text from 'sections' with given keys
+def get_sections_text(keys):
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  for key in keys:
+
+    for group in data['groups']:
+        for block in group['blocks']:
+            for section in block['sections']:
+
+                # Check we've actually found something
+                if section.get(key) != None and section.get(key) != '':
+                  the_list.append("sections: " + section.get(key))
+
+  return the_list
+
+
+
+
+# All text from 'questions' with given keys
+def get_questions_text(keys):
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  for key in keys:
+
+    for group in data['groups']:
+        for block in group['blocks']:
+            for section in block['sections']:
+                for question in section['questions']:
+
+                  # Check we've actually found something
+                  if question.get(key) != None and question.get(key) != '':
+                    the_list.append("questions: " + question.get(key))
+
+  return the_list
+
+
+# All text from 'answers' with given keys
+def get_answers_text(keys):
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  for key in keys:
+
+    for group in data['groups']:
+        for block in group['blocks']:
+            for section in block['sections']:
+                for question in section['questions']:
+                    for answer in question['answers']:
+
+                      # Check we've actually found something
+                      if answer.get(key) != None and answer.get(key) != '':
+                        the_list.append("answers" + answer.get(key))
+
+  return the_list
+
+
+# All text from 'messages' with given keys
+def get_validation_message_text():
+
+  # The list of strings we're going to build up
+  the_list = []
+
+  for group in data['groups']:
+      for block in group['blocks']:
+          for section in block['sections']:
+              for question in section['questions']:
+                  for answer in question['answers']:
+
+                    if 'validation' in answer:  # Ensure key is available!
+
+                      for key, value in answer['validation']['messages'].items():
+                          the_list.append("messages: " + value)
+
+  return the_list
+
+
+
+
+# Wrapper function which generates the output file
+def get_translatable_text():
+
+  keys = [
+    'description',
+    'guidance',
+    'label',
+    'title'
+  ]
+
+  header_text = get_header_text()
+  blocks_text = get_blocks_text(keys)
+  sections_text = get_sections_text(keys)
+  question_text = get_questions_text(keys)
+  validation_text = get_validation_message_text()
+
+  combined = header_text + blocks_text + sections_text + question_text + validation_text
+
+  # Convert to set to remove all duplicates
+  unique_text = set(header_text + blocks_text + sections_text + question_text + validation_text)
+
+  # Convert back to list to sort
+  sorted_list = list(unique_text)
+  sorted_list.sort()
+
+  return sorted_list
+
+
+
+
+def output_to_file(text_list):
+
+  # Output the list - this is just for testing! Please remove after!
+  for line in text_list:
+    print("%s" % line)
+
+  # Dump the output to a file
+  test_file = open('test.txt', 'w')
+
+  for line in text_list:
+    test_file.write("%s\n" % line)
+
+  test_file.close()
+
+
+
+def run():
+  text = get_translatable_text()
+
+  output_to_file(text)
+
+
+
+
+
+### Entry point for entire script for now...
+run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(json.dumps(data))
+# print data
+
+
+
+
+
+#     data["mime_type"] = 'something/test'
+#
+#     jsonData.seek(0)
+#
+#     target_file = open('small_new.json', 'w')
+#
+#     json_dump = json.d

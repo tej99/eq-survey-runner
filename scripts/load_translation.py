b006a27348e8bb0cc4c6d4d971a9c1fe6811932d
Darrell Cox <darrellcox@Darrells-MacBook-Pro.local>
2016-11-17 11:03:48 +0000

    fix merge

--- scripts/load_translation.py
+++
@@ -1,7 +0,0 @@
-
-
-text_file = open('/Users/darrellcox/projects/eq-survey-runner/scripts/test.txt', 'r', encoding="utf8")
-list1 = text_file.readlines()
-
-for value in list1:
-    print("%s" % value)

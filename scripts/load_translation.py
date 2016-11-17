048b60784138d8191aa141db3401df6a82005d3b
Darrell Cox <darrellcox@Darrells-MacBook-Pro.local>
2016-11-17 10:57:08 +0000

    start loading into json

--- scripts/load_translation.py
+++ scripts/load_translation.py
@@ -1,7 +1,7 @@
 
 
-text_file = open("/Users/darrellcox/projects/eq-survey-runner/app/data/test.txt","r")
+text_file = open('/Users/darrellcox/projects/eq-survey-runner/scripts/test.txt', 'r', encoding="utf8")
 list1 = text_file.readlines()
 
 for value in list1:
-    print ("%s") % (value),
+    print("%s" % value)

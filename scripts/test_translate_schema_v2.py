ec0f43bc4cde6e5b3a6d130bc7d39b556d2ed3b2
liamtoozer <liam.toozer@gmail.com>
2016-11-23 14:01:09 +0000

    Additional updates

--- scripts/test_translate_schema_v2.py
+++ scripts/test_translate_schema_v2.py
@@ -9,25 +9,37 @@
 - Will this be run from command line for now? Guard against different inputs.
 """
 
-
 import json
-import os.path
-import sys
+import os
+import click
 
-TEXT_SEPARATOR = "|"
-SCHEMA_DIR = "/Users/liamtoozer/projects/eq-survey-runner/app/data/"
+TEXT_SEPARATOR = "±"
+SCHEMA_DIR = os.path.expanduser('~') + "/projects/eq-survey-runner/app/data/"
+OUTPUT_DIR = os.path.expanduser('~') + "/OUTPUTS"
 
+print(os.path.expanduser('~'))
 
-def is_text_present(text, key):
-  return text.get(key) != None and text.get(key) != ''
+def get_text_for_container(container):
+  # extracted_text = []
+  extracted_text = ''
+
+  for key in ['description', 'guidance', 'label', 'title']:
+
+    value = container.get(key)
+
+    if value is not None and value != '':
+        # extracted_text.append(value)
+      extracted_text += value
+
+  return extracted_text
 
 
 # Builds and returns a list of translatable text from JSON file
-def get_text():
+def get_text(json_file_to_deserialise):
 
   # Get the JSON file
-  with open(file, 'r', encoding="utf8") as jsonData:
-    data = json.load(jsonData)
+  with open(json_file_to_deserialise, 'r', encoding="utf8") as json_data:
+    data = json.load(json_data)
 
   # Create the list of strings we're going to build up and assign 'header' text first
   translatable_text = [data['title'], data['description'], data['introduction']['description']]
@@ -36,38 +48,24 @@ def get_text():
     translatable_text.append(value)
 
 
-  # Keys we need to get text for
-  keys = [
-    'description',
-    'guidance',
-    'label',
-    'title'
-  ]
-
   # Now build up translatable text from the nested dictionaries and lists
-  for key in keys:
+  for group in data['groups']:
+    for block in group['blocks']:
+      translatable_text.append(get_text_for_container(block))
 
-    for group in data['groups']:
-      for block in group['blocks']:
-        if is_text_present(block, key):
-          translatable_text.append(block.get(key))
+      for section in block['sections']:
+        translatable_text.append(get_text_for_container(section))
 
-        for section in block['sections']:
-          if is_text_present(section, key):
-            translatable_text.append(section.get(key))
+        for question in section['questions']:
+          translatable_text.append(get_text_for_container(question))
 
-          for question in section['questions']:
-            if is_text_present(question, key):
-              translatable_text.append(question.get(key))
+          for answer in question['answers']:
+            translatable_text.append(get_text_for_container(answer))
 
-            for answer in question['answers']:
-                if is_text_present(answer, key):
-                  translatable_text.append(answer.get(key))
+            if 'validation' in answer:  # Ensure key is available!
 
-                if 'validation' in answer:  # Ensure key is available!
-
-                  for value in answer['validation']['messages'].values():
-                      translatable_text.append(value)
+              for value in answer['validation']['messages'].values():
+                translatable_text.append(value)
 
   return translatable_text
 
@@ -84,7 +82,6 @@ def sort_text(text_to_sort):
   return sorted_translatable_text
 
 
-
 def output_to_file(text_list):
 
   # Dump the output to a file
@@ -99,29 +96,61 @@ def output_to_file(text_list):
 
 
 
-def usage():
-    print()
-    print('Usage: python ' + os.path.basename(__file__) + ' <json_schema_file_name>')
-    exit(1)
 
 
-def check_file_exists(file_name):
+# @click.command()
+# @click.argument('json_file', type=click.Path(exists=True))
+# def main(json_file):
+#   """
+#   This script takes a JSON schema file and outputs all translatable text into
+#   a separate text file.
+#   """
+#   click.echo("JSON file '" + json_file + "'provided")
+#
+#   # json_file = SCHEMA_DIR + '1_0112.json'
+#
+#   click.echo('Creating list of translatable text from ' + json_file)
+#   text = get_text(json_file)
+#
+#   click.echo('Making list unique...')
+#   # unique_text = remove_duplicates(text)
+#
+#   click.echo('Sorting list...')
+#   # sorted_text = sort_text(unique_text)
+#   sorted_text = sort_text(text)
+#
+#   click.echo('Outputting list into file...')
+#   output_to_file(sorted_text)
+#
+#   click.echo('Script complete.\nOutput of translated text: ' + "OUTPUT TEXT FILE LOCATION")
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-    if not os.path.isfile(file_name):
-      print()
-      print('JSON file ' + '\'' + file_name + '\'' + ' not found.')
-      exit(2)
 
 
-
-### Entry point for entire script for now...
-# if len(sys.argv) < 2:
-#   usage()     # Incorrect number of args passed in
-
-
-# file = SCHEMA_DIR + sys.argv[1]
 file = SCHEMA_DIR + '1_0112.json'
-check_file_exists(file)
 
-output_to_file(sort_text(get_text()))
+text = get_text(file)
 
+sorted_text = sort_text(text)
+
+output_to_file(sorted_text)
+
+
+
+
+
+# def usage():
+#     print()
+#     print('Usage: python ' + os.path.basename(__file__) + ' <json_schema_file_name>')
+#     exit(1)
+#
+#
+# def check_file_exists(file_name):
+#
+#     if not os.path.isfile(file_name):
+#       print()
+#       print('JSON file ' + '\'' + file_name + '\'' + ' not found.')
+#       exit(2)

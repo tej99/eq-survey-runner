bdf21dc601d82bcbfa5fd809179cecf551e3cb9e
Andrew Maddaford <ajmaddaford@gmail.com>
2016-12-13 15:36:35 +0000

    Change translations to extract to Excel spreadsheet
    There are some Census specific changes to extract_translation_data.py that need to be worked on, committing this though as we need to extract the strings for Census to send off for translation?

--- translations/extract_translation_data.py
+++ translations/extract_translation_data.py
@@ -12,12 +12,12 @@
 import json
 import os
 import sys
+from openpyxl import Workbook
 
-TEXT_SEPARATOR = "±"
-OUTPUT_FILE_EXTENSION = "_translate.txt"
+OUTPUT_FILE_EXTENSION = "_translate.xlsx"
 
 
-def get_text_for_container(container):
+def get_text_for_container(container, context=None):
 
     extracted_text = []
 
@@ -26,11 +26,17 @@ def get_text_for_container(container):
             value = container.get(key)
 
             if value is not None and value != '':
-                extracted_text.append(value)
+                if context is not None:
+                    extracted_text.append((context, value))
+                else:
+                    extracted_text.append(('', value))
 
     elif isinstance(container, list):
         for value in container:
-            extracted_text.append(value)
+            if context is not None:
+                extracted_text.append((context, value))
+            else:
+                extracted_text.append(('', value))
 
     return extracted_text
 
@@ -39,47 +45,39 @@ def get_text(data):
     translatable_text = []
 
     # Get header section text
-    translatable_text.extend(get_text_for_container(data))
-    translatable_text.extend(get_conditional_text_for_container(data))
+    # translatable_text.extend(get_text_for_container(data))
+    # translatable_text.extend(get_conditional_text_for_container(data))
 
     # Now build up translatable text from the nested dictionaries and lists
     for group in data['groups']:
-        translatable_text.extend(get_text_for_container(group))
+        # translatable_text.extend(get_text_for_container(group))
 
         for block in group['blocks']:
-            translatable_text.extend(get_text_for_container(block))
+            # translatable_text.extend(get_text_for_container(block))
 
             for section in block['sections']:
-                translatable_text.extend(get_text_for_container(section))
-                translatable_text.extend(get_conditional_text_for_container(section))
+                translatable_text.extend(get_text_for_container(section, section['id']))
 
                 for question in section['questions']:
-                    translatable_text.extend(get_text_for_container(question))
-                    translatable_text.extend(get_conditional_text_for_container(question))
+                    translatable_text.extend(get_text_for_container(question, question['id']))
+                    translatable_text.extend(get_validation_text(question))
+                    translatable_text.extend(get_guidance_text(question))
 
                     for answer in question['answers']:
-                        translatable_text.extend(get_text_for_container(answer))
-                        translatable_text.extend(get_conditional_text_for_container(answer))
+                        translatable_text.extend(get_text_for_container(answer, answer['id']))
+                        translatable_text.extend(get_guidance_text(answer))
+                        translatable_text.extend(get_options_text(answer))
+                        translatable_text.extend(get_validation_text(answer))
 
     return translatable_text
-
-
-def get_conditional_text_for_container(container):
-    extracted_text = []
-
-    extracted_text.extend(get_introduction_text(container))
-    extracted_text.extend(get_options_text(container))
-    extracted_text.extend(get_guidance_text(container))
-    extracted_text.extend(get_validation_text(container))
-
-    return extracted_text
 
 
 def get_validation_text(container):
     extracted_text = []
     if 'validation' in container:
         for value in container['validation']['messages'].values():
-            extracted_text.append(value)
+            context = container['id'] + ' [validation message]'
+            extracted_text.append((context, value))
 
     return extracted_text
 
@@ -91,13 +89,13 @@ def get_guidance_text(container):
         guidance_text = container['guidance']
 
         if isinstance(guidance_text, str):
-            extracted_text.append(container['guidance'])
+            extracted_text.append((container['id'] + ' [answer guidance]', container['guidance']))
         else:
             for guidance in container['guidance']:
-                extracted_text.extend(get_text_for_container(guidance))
+                extracted_text.extend(get_text_for_container(guidance, container['id'] + ' [question guidance]'))
 
                 if 'list' in guidance:
-                    extracted_text.extend(get_text_for_container(guidance['list']))
+                    extracted_text.extend(get_text_for_container(guidance['list'], container['id'] + ' [question guidance]'))
 
     return extracted_text
 
@@ -106,10 +104,10 @@ def get_options_text(container):
     extracted_text = []
     if 'options' in container:
         for options in container['options']:
-            extracted_text.extend(get_text_for_container(options))
+            extracted_text.extend(get_text_for_container(options, container['id']))
 
             if 'other' in options:
-                extracted_text.extend(get_text_for_container(options['other']))
+                extracted_text.extend(get_text_for_container(options['other'], container['id']))
 
     return extracted_text
 
@@ -127,22 +125,27 @@ def get_introduction_text(container):
     return extracted_text
 
 
-def sort_text(text_to_sort):
-    sorted_text = list(text_to_sort)
-    sorted_text.sort(key=len, reverse=True)
-
-    return sorted_text
+def sort_text(tuples_to_sort):
+    return sorted(tuples_to_sort, key=lambda tup: tup[0], reverse=True)
 
 
 def remove_duplicates(text_with_duplicates):
     return set(text_with_duplicates)
 
 
-def output_text_to_file(text_list, file_name):
-    with open(file_name, 'w', encoding="utf8") as output_file:
-
-        for line in text_list:
-            output_file.write("%s" % line + TEXT_SEPARATOR + "\r\n")
+def output_translations_to_file(text_list, file_name):
+    wb = Workbook()
+    ws = wb.active
+    ws.append(['Context', 'English Text', 'Welsh Text'])
+    for line in text_list:
+        if 'section' in line[0]:
+            ws.append(['', '', ''])
+        value = line[1]
+        if value == '{{[answers.first_name[group_instance], answers.last_name[group_instance]] | format_household_name }}' or \
+                value == '{{[answers.first_name, answers.last_name] | format_household_name }}':
+            value = ''
+        ws.append([line[0], value, ' '])
+    wb.save(file_name)
 
 
 def strip_directory_and_extension(file):
@@ -182,12 +185,12 @@ def command_line_handler(json_file, output_directory):
     text = get_text(deserialised_json)
 
     print('Removing duplicate text...')
-    unique_text = remove_duplicates(text)
-    sorted_text = sort_text(unique_text)
+    # unique_text = remove_duplicates(text)
+    sorted_text = sort_text(text)
 
     print('Outputting text to file...')
     output_file_name = create_output_file_name_with_directory(output_directory, json_file)
-    output_text_to_file(sorted_text, output_file_name)
+    output_translations_to_file(sorted_text, output_file_name)
 
     print('Finished successfully.')
     print()

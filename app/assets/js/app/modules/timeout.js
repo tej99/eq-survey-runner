30fc5740e29c3d57dad7b2ad04b90e9adab28856
Hamish Taplin <hello@hamishtaplin.com>
2017-02-06 12:28:22 +0000

    implemeted countdown

--- app/assets/js/app/modules/timeout.js
+++ app/assets/js/app/modules/timeout.js
@@ -1,37 +1,58 @@
 import domready from './domready'
 import A11yDialog from 'a11y-dialog'
+import { padStart } from 'lodash'
 
 domready(() => {
   const timeLimit = 120 // seconds
-  const timeLeft = 86 // seconds
+  const magicRadius = 57.5 // magic number
   const dialogEl = document.querySelector('.js-timeout-dialog')
-  const path = document.querySelector('.circle')
+  const circle = document.querySelector('.js-timeout-circle')
   const timeText = document.querySelector('.js-timeout-time')
 
   const dialog = new A11yDialog(dialogEl)
 
-  if (path) {
-    const length = path.getTotalLength()
+  let timeLeft = 120 // seconds
 
-    const setTime = (t) => {
-      path.style.strokeDashoffset = length / (t / timeLimit)
+  if (circle) {
+    const circleRadius = circle.getAttribute('r')
+    const stroke = circle.getAttribute('stroke-width')
 
-      const minutes = "00"
-      const seconds = "20"
+    circle.style.strokeDasharray = '0, 1000'
+    circle.getBoundingClientRect()
 
-      timeText.innerHTML = `${minutes}:${seconds}`
+    const setTime = (time) => {
+      const date = new Date(null)
+      date.setSeconds(time)
+      const mins = padStart(date.getUTCMinutes(), 2, '0')
+      const seconds = padStart(date.getUTCSeconds(), 2, '0')
+
+      timeText.innerHTML = `${mins}:${seconds}`
+
+      const angle = (360 - (360 / (timeLimit / time))) / (magicRadius / circleRadius)
+
+      if (time < 10) {
+        timeText.classList.add('is-warning')
+      }
+
+      if (angle > stroke) {
+        // adjust for stroke, but add a couple of px to account for stroke radius
+        circle.style.strokeDasharray = (angle - (stroke / 2)) + 2 + ', 1000'
+        circle.getBoundingClientRect()
+      }
     }
 
-    path.style.strokeDasharray = length + ' ' + length
-    path.style.strokeDashoffset = length
-    path.style.strokeDashoffset = length
-    path.style.strokeLinecap = 'round'
-    path.getBoundingClientRect()
-
-    window.setTimeout(() => {
+    const t = window.setInterval(() => {
+      timeLeft = timeLeft - 1
       setTime(timeLeft)
-    }, 100)
-  }
 
-  dialog.show()
+      if (timeLeft <= 120) {
+        dialog.show()
+      }
+
+      if (timeLeft < 1) {
+        // dialog.hide()
+        window.clearInterval(t)
+      }
+    }, 1000)
+  }
 })

e15b02ef1ce2269cb6fb8928df6985859c8219b3
Yoganand Kunche <kumar.kunche@gmail.com>
2017-02-09 13:26:33 +0000

    added selenium test to cover defect 923

--- tests/functional/pages/surveys/multiple-choice.page.js
+++ tests/functional/pages/surveys/multiple-choice.page.js
@@ -7,6 +7,16 @@ class MultipleChoiceWithOtherPage extends QuestionPage {
     return this
   }
 
+  clickBacon(){
+    browser.element('[name ="ca3ce3a3-ae44-4e30-8f85-5b6a7a2fb23c"]').click()
+    return this
+  }
+
+  clickCheese(){
+    browser.element('[name ="ca3ce3a3-ae44-4e30-8f85-5b6a7a2fb23c"]').click()
+    return this
+  }
+
   otherInputFieldExists() {
     return browser.isExisting('[data-qa="other-option"]')
   }
@@ -20,6 +30,14 @@ class MultipleChoiceWithOtherPage extends QuestionPage {
     return this
   }
 
+  getOtherInputField() {
+    return browser.element('[data-qa="other-option"]').getValue()
+  }
+
+  clickTopprevious() {
+    browser.element('a[id="top-previous"]').click()
+    return this
+  }
 }
 
 export default MultipleChoiceWithOtherPage
